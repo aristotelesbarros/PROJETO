43c2babfacdcdd98abb26fe80a25a2fad1b13100
aristotelesbarros <143902784+aristotelesbarros@users.noreply.github.com>
2024-02-27 11:18:21 -0300

    att3

--- scripts.js
+++ scripts.js
@@ -1,29 +1,15 @@
 const modal_overlay = document.querySelector ('.modal_overlay');
 const cards = document.querySelectorAll ('.card');
-for (let card of cards){
-    card.addEventListener ("click", function(){
-        const videoid=card.getAttribute ("id");
+
+ for (let card of cards ) {
+    card.addEventListener ("click",function(){
+        const videoId = card.getAttribute ("id");
         modal_overlay.classList.add('active')
-        modal_overlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoid}`;
+        modal_overlay.querySelector('iframe').src =`https://www.youtube.com/embed/${videoId}`
     })
-}
-document.querySelector ('.close_modal').addEventListener("click",function(){
-    modal_overlay.classList.remove("active")
-    modal_overlay.requerySelector("iframe").src="";
-})
-let count= 1;
-document.getElementById("radio1").checked = true;
-setInterval(function(){
-    nextimage();
+ };
 
-}, 2000 )
-
-function nextimage(){
-    count++;
-    if(count>4){
-        count = 1;
-
-    }
-    document.getElementById("radio"+count).checked = true;
-    
-}
+ document.querySelector('.close_modal').addEventListener("click",function(){
+    modal_overlay.classList.remove ('active')
+    modal_overlay.querySelector('iframe').src =""
+ });
